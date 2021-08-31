d1d57fe335e3a6e8ca0748678fca8bda7722b832
mahavinay <mahavinay28@gmail.com>
2021-08-31 17:48:30 +0200

    Added more test, working on failed test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,23 @@
-import { render, screen, cleanup, getAllByTestId, findAllByTestId, mount } from '@testing-library/react';
-import { shallow } from 'enzyme';
+import {
+  render,
+  screen,
+  cleanup,
+  getAllByTestId,
+  getByTestId,
+  findAllByTestId,
+  mount,
+  fireEvent,
+} from "@testing-library/react";
+import { shallow } from "enzyme";
 import React from "react";
 import { unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
-import Pagination from "../src/components/Pagination"
-import bookData from './assets/books.data.json'
+import Pagination from "../src/components/Pagination";
+import bookData from "./assets/books.data.json";
 
-import App from './App';
-import BookListItem from './components/BookListItem';
-import { ReactWrapper } from 'enzyme';
+import App from "./App";
+import BookListItem from "./components/BookListItem";
+import { ReactWrapper } from "enzyme";
 
 let container = null;
 beforeEach(() => {
@@ -17,95 +26,120 @@ beforeEach(() => {
 });
 
 afterEach(() => {
-  // cleanup on exiting
   unmountComponentAtNode(container);
   container.remove();
   container = null;
 });
 
-test('Render App', () => {
+test("Render App", () => {
   render(<App />);
 });
 
-test('Render Pagination Component', () => {
+test("Render Pagination Component amd check for the title ", () => {
   act(() => {
     render(<Pagination data={bookData} booksPerPage={5} />);
   });
-  const h1 = document.querySelector('h1').textContent;
-  expect(h1).toBe('Display List of Books');
- 
+  const h1 = document.querySelector("h1").textContent;
+  expect(h1).toBe("Display List of Books");
 });
 
-test('Display page numbers ', () => {
-  const { findAllByTestId } = render(<Pagination data={bookData} booksPerPage={5}/>);
-  expect(findAllByTestId('testPageNumber')).toBeTruthy();
+test("Display page numbers ", () => {
+  const { findAllByTestId } = render(
+    <Pagination data={bookData} booksPerPage={5} />
+  );
+  expect(findAllByTestId("testPageNumber")).toBeTruthy();
 });
 
 test("Is Previous Button is present in the page ", () => {
-  const { getByTestId } = render(<Pagination data={bookData} booksPerPage={5}/>);
-  expect(getByTestId('btnPrev')).toBeTruthy();
+  const { getByTestId } = render(
+    <Pagination data={bookData} booksPerPage={5} />
+  );
+  expect(getByTestId("btnPrev")).toBeTruthy();
 });
 
-test('Is Previous Button Button Disabled', ()=> {
-  const { getByText } = render(<Pagination data={bookData} booksPerPage={5}/>);
-  expect(getByText(/Prev/i).closest('button')).toBeDisabled();  
+test("Is Previous Button Button Disabled", () => {
+  const { getByText } = render(<Pagination data={bookData} booksPerPage={5} />);
+  expect(getByText(/Prev/i).closest("button")).toBeDisabled();
 });
 
 test("Is Next Button present in the page ", () => {
-   const { getByTestId  } = render(<Pagination data={bookData} booksPerPage={5}/>);
-  expect(getByTestId('btnNext')).toBeTruthy();
+  const { getByTestId } = render(
+    <Pagination data={bookData} booksPerPage={5} />
+  );
+  expect(getByTestId("btnNext")).toBeTruthy();
 });
 
 test("renders book data", async () => {
   const fakeBook = {
     isbn: 12345,
-    title : "Harry Potter",
-    pageCount : 202,
-    status : "PUBLISH",
-    thumbnailUrl : "http://s3.amazonaws.com/QQQERTRT.book-thumb-images/image.jpg"
-
+    title: "Harry Potter",
+    pageCount: 202,
+    status: "PUBLISH",
+    thumbnailUrl:
+      "http://s3.amazonaws.com/QQQERTRT.book-thumb-images/image.jpg",
   };
   jest.spyOn(global, "fetch").mockImplementation(() =>
     Promise.resolve({
-      json: () => Promise.resolve(fakeBook)
+      json: () => Promise.resolve(fakeBook),
     })
   );
-//   // global.fetch.mockRestore();
 });
 
-test("Update state on click", () => {
-  const onChange = jest.fn();
-  act(() => {
-    ReactDOM.render(<Pagination data={bookData} booksPerPage={5}/>, container);
-  });
+// test("Click on Page and check for state change", () => {
+//   const onChange = jest.fn();
+//   act(() => {
+//     render(<Pagination data={bookData} booksPerPage={5} />, container);
+//   });
 
-  const element = screen.findAllByTestId('testPageNumber')
-  
-  act(() => {
-    element.dispatchEvent(new MouseEvent('click', {bubbles:true}));
-  });
-  
-  // const wrapper =  mount(<Pagination data={bookData} booksPerPage={5}/>)
-  // const handleClick = jest.spyOn(React, "useState");
-  // handleClick.mockImplementation(presentPage =>  [currentPage, setcurrentPage]);
-  // wrapper.find('[data-testid="testPageNumber"]').simulate('click');
-  // expect(onChange).toBeTruthy();
+//   const element = document.querySelector("[data-testid=testPageNumber-1]");
+//   expect(element.innerHTML).toBe("1");
 
+//   act(() => {
+//     element.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+//   });
 
-  // // get a hold of the button element, and trigger some clicks on it
-  // const button = document.querySelector("[data-testid='testPageNumber']");
+//   expect(onChange).toHaveBeenCalledTimes(1);
+//   expect(element.firstChild.classList.contains("active")).toBe(true);
 
-  // act(() => {
-  //   button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
-  // });
+//   //   act(() => {
+//   //     for (let i = 0; i < 5; i++) {
+//   //       button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+//   //     }
+//   //   });
 
-  // expect(onChange).toHaveBeenCalledTimes(1);
+//   //   expect(onChange).toHaveBeenCalledTimes(6);
+// });
 
-  // act(() => {
-  //   for (let i = 0; i < 5; i++) {
-  //     button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
-  //   }
-  // });
+test("Previous button is disbled when page is loaded", () => {
+  const { getByText } = render(<Pagination data={bookData} booksPerPage={5} />);
+  expect(getByText(/Prev/i).closest("button")).toHaveAttribute("disabled");
+});
 
-  // expect(onChange).toHaveBeenCalledTimes(6);
-})
+test("Previous button should not be enabled -- negative test case", () => {
+  const { getByText } = render(<Pagination data={bookData} booksPerPage={5} />);
+  expect(getByText(/Prev/i).closest("button")).not.toHaveAttribute("disabled");
+});
+
+test("Next button should be enabled", () => {
+  const { getByText } = render(<Pagination data={bookData} booksPerPage={5} />);
+  expect(getByText(/Next/i).closest("button")).not.toHaveAttribute("disabled");
+});
+
+test("active page test after click event", () => {
+  const { getByTestId } = render(
+    <Pagination data={bookData} booksPerPage={5} />
+  );
+
+  // const containerElement = getByTestId("testPageNumber-1");
+
+  // const containerElement = document.querySelector(
+  //   "[data-testid='testPageNumber-2']"
+  // );
+  // fireEvent.click(containerElement);
+  // expect(containerElement.classList.contains("active")).toBe(true);
+
+  const containerElement = getByTestId("testPageNumber-2");
+  fireEvent.click(containerElement);
+
+  expect(containerElement.classList.contains("active")).toBe(true);
+});
